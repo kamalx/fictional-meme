23a7a19fc4279df15e2d8579bc3e712e5353c13d
Kamal Morjal <kamal@morjal.com>
2024-12-24 05:21:22 +0530

    Add and fix app nav

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <main className="foo">
+      <p>Cusotmers page</p>
+    </main>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <main className="foo">
+      <p>Dashboard page</p>
+    </main>
+  )
+}
