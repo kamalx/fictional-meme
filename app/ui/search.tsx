9191ae886b93d591633bf7dea07bd4be35ddc919
Kamal Morjal <kamal@morjal.com>
2024-12-25 04:50:08 +0530

    Search and Pagination :mage_man: :fairy: :mermaid: :santa: :genie: :alien: :ghost:

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) =>  {
     // `URLSearchParams` is a Web API that provides utility methods for
     // manipulating the URL query parameters. Instead of creating a
     // complex string literal, you can use it to get the params string
@@ -26,8 +27,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathname}?${params.toString()}`)
 
     console.log(params, params.size)
-    console.log(term)
-  }
+    console.log(`Searching ${term}...`)
+  }, 1000)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
